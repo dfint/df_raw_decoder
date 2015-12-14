f8b77ae2f1714c8f67d0dfdee525daa6001d5b19
kateBy <nonameisiam@yandex.ru>
2015-12-14 22:04:52 +0300

    Добавлена функция рекурсивного обхода и декодирования файлов. Правлен .gitignore

--- decode_raw_dwarf_fortress.py
+++ decode_raw_dwarf_fortress.py
@@ -1,6 +1,10 @@
+#/usr/bin/python3.4
 
 import zlib
 from io import BytesIO
+import os
+from os.path import join as joinPath
+from os.path import exists
 
 int_from_bytes = lambda x: int.from_bytes(x, byteorder="little")
 from_int16 = lambda x: x.to_bytes(2, byteorder="little")
@@ -21,7 +25,15 @@ def decode_datafile(zipfile, txtfile):
         buf.seek(0)
         lines_count = int_from_bytes(buf.read(4)) #Первые 4 байта - кол-во строк
 
+
+            
+
         result = []
+        
+        file_path, fn = os.path.split(zipfile)
+        indexFile = False
+        if fn == 'index': #Файл index имеет туже структуру, но немного "зашифрован"
+            indexFile = True
 
         for line in range(lines_count):
             _len = int_from_bytes(buf.read(4)) #Длина строки
@@ -29,11 +41,14 @@ def decode_datafile(zipfile, txtfile):
             if _len != _len2:
                 print("Некорректная длина в строке:", line)
             _str = buf.read(_len)
-            
-            result.append(_str)
 
+            if indexFile:
+                decoded = [255-(i%5)-c for i,c in enumerate(_str)]
+                result.append(bytes(decoded))
+            else:
+                result.append(_str)
+   
         open(txtfile, 'wb').write(b"\n".join(result))
-            
          
     else:
         print('Некорректная длина файла', filename)
@@ -60,15 +75,36 @@ def encode_datafile(txtfile, zipfile, encoding="cp1251"):
     _zip.write(deflate)
     _zip.close()
     
-    
-if __name__ == "__main__":
 
-    zipfile = '/home/user/Games/df_linux/data/help/a_first'
+def decode_all_files(directory=""):
+    dataPath = joinPath(directory, 'data') #Исходная директория
+    if not exists(dataPath):
+        print("Не найден каталог 'data'")
+        #return False
 
-    decode_datafile(zipfile, "/tmp/a_first")
-    encode_datafile("/tmp/a_first","/tmp/a_first_1")
-    decode_datafile("/tmp/a_first_1", "/tmp/a_first_2")
-    encode_datafile("/tmp/a_first_2","/tmp/a_first_3")
-    decode_datafile("/tmp/a_first_3", "/tmp/a_first_4")
+    DATA_SRC = 'data_src'
+
+    data_src_path = joinPath(directory, DATA_SRC)
+
+    #Получаем список всех файлов, у которых нет расширения
+    all_files = []
+    for root, directories, files in os.walk(dataPath):
+        for file in files:
+            fn, ext = os.path.splitext(file)
+            if ext == "":
+                new_path = root.replace('data',data_src_path)
+                if not exists(new_path):
+                    os.mkdir (new_path)
+                print(joinPath(root,file), "...OK")
+                decode_datafile(joinPath(root,file), joinPath(new_path,file))
+            
+
+
+decode_all_files()
+
+
+
+
+
 
 
