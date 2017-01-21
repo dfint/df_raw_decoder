c1f3a766829d99cabede15f51cb52ecb2b103a60
insolor <insolor@gmail.com>
2017-01-21 20:09:45 +0300

    (now for real) Add basic setup.py, make df_raw_decoder.py not to exit immediately if it is imported

--- df_raw_decoder.py
+++ df_raw_decoder.py
@@ -131,63 +131,61 @@ func = {"directory":{"--decode":decode_directory,"--encode":encode_directory,
 
 import sys
 
-if __name__ != '__main__':
-    exit()
-    
-if len(sys.argv) < 4:
-    print(usage)
-    exit()
+if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print(usage)
+        exit()
 
-action = ""
-overwrite = False
-frompath = sys.argv[-2]
-topath = sys.argv[-1]
+    action = ""
+    overwrite = False
+    frompath = sys.argv[-2]
+    topath = sys.argv[-1]
 
-for option in sys.argv[1:-2]:
-    if option in ["--decode", "--encode", "-d", "-e"]:
-        action = option
-    elif option in ["--yes", "-y"]:
-        overwrite = True
+    for option in sys.argv[1:-2]:
+        if option in ["--decode", "--encode", "-d", "-e"]:
+            action = option
+        elif option in ["--yes", "-y"]:
+            overwrite = True
 
-frompath = realpath(frompath)
-topath   = realpath(topath)
+    frompath = realpath(frompath)
+    topath   = realpath(topath)
 
-print("action:", action)
-print("overWrite:",overwrite)
-print("frompath:", frompath)
-print("topath:", topath)
+    print("action:", action)
+    print("overWrite:",overwrite)
+    print("frompath:", frompath)
+    print("topath:", topath)
 
 
-if action:
-    if exists(frompath):
-        if isdir(frompath):
-            #Если цель - каталог
-            if exists(topath):
-                if not overwrite:
-                    answer = input("Directory %s already exists, overwrite? [y/N] " % topath)
-                    if not (answer in ["y","Y"]):
-                        print("Interrupted by the user")
-                        exit()
-            
-            #Обработка каталога, в зависимости от выбранного действия
-            func["directory"][action](frompath, topath)
+    if action:
+        if exists(frompath):
+            if isdir(frompath):
+                #Если цель - каталог
+                if exists(topath):
+                    if not overwrite:
+                        answer = input("Directory %s already exists, overwrite? [y/N] " % topath)
+                        if not (answer in ["y","Y"]):
+                            print("Interrupted by the user")
+                            exit()
                 
-        elif isfile(frompath):
-            #Если цель - один файл
-            if exists(topath):
-                if not overwrite:
-                    answer = input("File %s already exists, overwrite? [y/N] " % topath)
-                    if not (answer in ["y","Y"]):
-                        print("Interrupted by the user")
-                        exit()
+                #Обработка каталога, в зависимости от выбранного действия
+                func["directory"][action](frompath, topath)
                     
-            #Обработка файла, в зависимости от выбранного действия
-            func["file"][action](frompath, topath)
+            elif isfile(frompath):
+                #Если цель - один файл
+                if exists(topath):
+                    if not overwrite:
+                        answer = input("File %s already exists, overwrite? [y/N] " % topath)
+                        if not (answer in ["y","Y"]):
+                            print("Interrupted by the user")
+                            exit()
+                        
+                #Обработка файла, в зависимости от выбранного действия
+                func["file"][action](frompath, topath)
+            else:
+                print("File type not recognized")
         else:
-            print("File type not recognized")
+            print("Given path of the source directory doesn't exist")
+        
     else:
-        print("Given path of the source directory doesn't exist")
-    
-else:
-    print(usage)
-    exit()
+        print(usage)
+        exit()
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(name='df_raw_decoder',
+      version='0.1',
+      description='DF raw decoder package forked from github.com/kateby/df_raw_decoder',
+      url='https://github.com/insolor/df_raw_decoder',
+      author='insolor',
+      author_email='insolor@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      zip_safe=False)
