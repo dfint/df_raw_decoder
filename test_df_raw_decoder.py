51885932434cc273a1faa0d5b04a264983db9b09
insolor <insolor@gmail.com>
2022-06-23 19:53:32 +0300

    Fixes, add black, reformat, update dependencies

--- test_df_raw_decoder.py
+++ test_df_raw_decoder.py
@@ -5,15 +5,21 @@ from hypothesis import given, strategies as st
 import pytest
 
 from df_raw_decoder import (
-    decode_data, DecodingException, encode_to_int32, encode_to_int16, encode_data,
-    encode_decode_index_file_line, pack_data, unpack_data
+    decode_data,
+    DecodingException,
+    encode_to_int32,
+    encode_to_int16,
+    encode_data,
+    encode_decode_index_file_line,
+    pack_data,
+    unpack_data,
 )
 
 
 def test_invalid_file():
     file = BytesIO()
-    file.write(100 .to_bytes(4, 'little'))
-    file.write(b'\0')
+    file.write((100).to_bytes(4, "little"))
+    file.write(b"\0")
     file.seek(0)
 
     with pytest.raises(DecodingException):
@@ -31,12 +37,13 @@ def test_encode_decode(lines):
     assert list(decode_data(BytesIO(encode_data(lines, True)), True)) == lines
 
 
-def test_invalid_line_length():
+@given(st.text())
+def test_invalid_line_length(text):
     buf = BytesIO()
 
-    buf.write(1 .to_bytes(4, 'little'))
+    buf.write((1).to_bytes(4, "little"))
 
-    line = b'test'
+    line = text.encode("utf-8")
     buf.write(encode_to_int32(len(line)))
     buf.write(encode_to_int16(len(line) + 1))
 
@@ -47,14 +54,17 @@ def test_invalid_line_length():
         next(decode_data(BytesIO(file)))
 
 
-@pytest.mark.parametrize("encoded,decoded", [
-    (b'\x96\x90\x99\x97\x83', b'index'),
-    (
-        b'\xcd\xce\x7f\xac\x89\x90\x97\x8b\x9b\x8e\x92\x99\x99\xdc\x99\x86\xde\xa9\x9b\x89\x91\xde\xbc\x98\x9a\x92\x8b',
-        b'20~Programmed by Tarn Adams'
-    ),
-    (b'\xcd\xca\x7f\xb8\x84\x9e\x8c\x97\xdc\xb5\x90\x8c\x89\x8a\x96\x8c\x8b', b'24~Dwarf Fortress'),
-])
+@pytest.mark.parametrize(
+    "encoded,decoded",
+    [
+        (b"\x96\x90\x99\x97\x83", b"index"),
+        (
+            b"\xcd\xce\x7f\xac\x89\x90\x97\x8b\x9b\x8e\x92\x99\x99\xdc\x99\x86\xde\xa9\x9b\x89\x91\xde\xbc\x98\x9a\x92\x8b",
+            b"20~Programmed by Tarn Adams",
+        ),
+        (b"\xcd\xca\x7f\xb8\x84\x9e\x8c\x97\xdc\xb5\x90\x8c\x89\x8a\x96\x8c\x8b", b"24~Dwarf Fortress"),
+    ],
+)
 def test_decode_encode_index_file_line(encoded, decoded):
     assert encode_decode_index_file_line(encoded) == decoded
     assert encode_decode_index_file_line(encode_decode_index_file_line(encoded)) == encoded
